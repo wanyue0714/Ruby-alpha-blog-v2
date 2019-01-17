a5b464bb0e7550bd97b6bb01b13f6daaf42ac043
wanyue0714 <wanyueglyr@outlook.com>
2019-01-17 17:08:56 -0500

    add home page and about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  # get 'pages/home', to: 'pages#home'
+  root 'pages#home'
+  # get 'pages/about', to: 'pages#about'
+  get 'about', to: 'pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
