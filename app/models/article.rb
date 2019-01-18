72b20c7ac4e2ccd51fdee6ada238ae38a308ada1
wanyue0714 <wanyueglyr@outlook.com>
2019-01-18 12:55:46 -0500

    create articles table and model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ActiveRecord::Base
-
+  # verify data before hit the database
+  validates :title, presence: true, length: { minimum: 3, maximum: 50}
+  validates :description, presence: true, length: { minimum: 10, maximum: 300}
 end
