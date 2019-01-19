74e15a0c256fdf373af3acb12c0575c6575fe21f
wanyue0714 <wanyueglyr@outlook.com>
2019-01-19 14:55:58 -0500

    fix bugs in article_controller redirect, show article successfully

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,20 +1,38 @@
 class ArticlesController < ApplicationController
+
   def new
     @article = Article.new
   end
 
+  # create article
   def create
-    render plain: params[:article].inspect
+    # render plain: params[:article].inspect
     # create a new instant to save content
     @article = Article.new(article_params)
-    @article.save
-    # then redirect to a display page
-    redirect_to articles_show(@article)
+    # after save
+    if @article.save
+      # display it on the layouts application page, means for all pages
+      flash[:notice] = "Article was successfully created"
+      # then redirect to a display page
+      redirect_to article_path(@article)
+    else
+      # stay at the same page, and empty the text boxes => reload the page
+      render 'new'
+    end
   end
+
+  # display article according to id
+  def show
+    @article = Article.find(params[:id])
+  end
+
   # this is a private function for passing parameters to create method
   private
-    def article_params
-      # it's a key value pair, key is the article, value is title and description
-      params.require(:article).permit(:title, :description)
-    end
+  # Remember where you put private keyword. all the methods below that, will become private
+  def article_params
+    # it's a key value pair, key is the article, value is title and description
+    params.require(:article).permit(:title, :description)
+  end
+
+
 end
