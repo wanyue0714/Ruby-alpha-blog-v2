06e326c1204789a5c2f780f2d02e1d24556d8623
wanyue0714 <wanyueglyr@outlook.com>
2019-01-28 15:19:38 -0500

    add restriction to articles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in
+    # if current_user return true, then it returns logged in
+    !!current_user
+  end
+
+  def require_user
+    # if user is logged out, then do actions below
+    if !logged_in
+      flash[:notice] = "You must be logged in to perform this action!"
+      redirect_to root_path
+    end
+  end
+
+
 end
