2907bc765ec908dbedd527d2c94afe7a33c8ffd9
wanyue0714 <wanyueglyr@outlook.com>
2019-01-29 15:32:45 -0500

    remove user new function from require_user before action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
 
-  before_action :require_user, except: [:index, :show]
+  before_action :require_user, except: [:new, :index, :show]
   before_action :require_same_user, only: [:edit, :update]
 
   def index
